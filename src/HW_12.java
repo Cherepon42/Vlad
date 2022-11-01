d11b20275a4907ed062f3a2fce849b45adb7aff2
Vlad <UladCherap@yandex.by>
2022-11-01 12:37:52 +0300

    HW_12,13

---
+++ src/HW_12.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class HW_12 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("resources/romeo-and-juliet.txt/");
+        System.out.println(file.canRead());
+
+    }
+}
