74a764d7a22030e5606cbc5e1fbb1ac0dbf13a10
Vlad <UladCherap@yandex.by>
2022-11-01 13:22:24 +0300

    Finished HW_12.java

--- src/HW_12.java
+++ src/HW_12.java
@@ -1,9 +1,39 @@
 import java.io.*;
 
 public class HW_12 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("resources/romeo-and-juliet.txt/");
-        System.out.println(file.canRead());
-
+        String maxStr = "";
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String str;
+            while (reader.ready()) {
+                str = reader.readLine();
+                str = str.replace(",", " ");
+                str = str.replace(".", " ");
+                str = str.replace("=", " ");
+                str = str.replace("'", " ");
+                str = str.replace("--", " ");
+                str = str.replace("?", " ");
+                str = str.replace("\t", " ");
+                str = str.replace("!", " ");
+                String[] strings = str.split(" ");
+                for(String nextLine : strings) {
+                    nextLine = nextLine.trim(); //System.out.println(nextLine);
+                    if(maxStr.length() < nextLine.length()) { System.out.println(nextLine);
+                        maxStr = nextLine;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //neighbor-stained
+        File fileResult = new File("resources/result.txt");
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileResult))) {
+            writer.write(maxStr);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
