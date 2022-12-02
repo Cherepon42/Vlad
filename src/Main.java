0c85169802f31606f614e35c436259e27f2b3b37
Vlad <UladCherap@yandex.by>
2022-12-02 12:00:32 +0300

    Object`s methods

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         User user1 = new User("Vlad", "KKK");
         User user2 = (User) user1.clone();
         System.out.println(user1.equals(user2));
+        System.out.println(user1.hashCode());
+        System.out.println(user1);
     }
 }
